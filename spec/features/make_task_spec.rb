3835bb59e3104e3b76f079b87f0c72a1e05af415
Ben <ben@leipeshit.com>
2016-04-22 02:34:30 +0200

    added scenario to test count

--- spec/features/make_task_spec.rb
+++ spec/features/make_task_spec.rb
@@ -1,27 +1,53 @@
 require 'rails_helper'
 
 feature 'Make a new task', :js => true do
-  scenario 'We can add a new task by entering its description in the textfield and pressing the enter key' do
-    #point your browser towards the todo path
+  before(:each) do
     visit todos_path
-    #enter description in the textfield
-    fill_in 'new-todo', with: 'NEW TASK'
-    #press enter (submit the form)
-    page.execute_script("$('form').submit()")
-
-    #now test #2 A new task is displayed in the list of tasks
-    expect(page).to have_content('NEW TASK')
+  end
+  scenario 'We can add a new task by entering its description in the textfield and pressing the enter key' do
+    make_new_task 'All your base are belong to us'
+    expect(page).to have_content('All your base are belong to us')
   end
   scenario 'counts change' do
-    make_new_task 'i can has cheeseburger'
-    expect(page).to have_content('i can has cheeseburger')
+    make_new_task 'i can has cheeseburger?'
     expect( page.find(:css, 'span#todo-count').text ).to eq "1"
-    #click the content
-    #it should get the state done
+    make_new_task 'candy mountain'
+    expect( page.find(:css, 'span#todo-count').text ).to eq "2"
   end
-
+  scenario 'counts change on check box' do
+    make_new_task 'candy mountain'
+    check('todo-1')
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+  end
+  scenario 'counts change on click label' do
+    make_new_task 'i can has cheeseburger?'
+    make_new_task 'candy mountain'
+    find("label[for=todo-1]").click
+    expect( page.find(:css, 'span#todo-count').text ).to eq "1"
+  end
+  scenario 'counts change on finish' do
+    make_new_task 'i can has cheeseburger?'
+    make_new_task 'candy mountain'
+    make_new_task 'over 9000'
+    expect( page.find(:css, 'span#todo-count').text ).to eq "3"
+    find("label[for=todo-1]").click
+    find("label[for=todo-2]").click
+    expect( page.find(:css, 'span#todo-count').text ).to eq "1"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "2"
+    expect( page.find(:css, 'span#total-count').text ).to eq "3"
+  end
+  scenario 'we can clear up tasks' do
+    make_new_task 'i can has cheeseburger?'
+    make_new_task 'candy mountain'
+    make_new_task 'over 9000'
+    find("label[for=todo-1]").click
+    find("label[for=todo-2]").click
+    find("#clean-up").click
+    expect( page.find(:css, 'span#total-count').text ).to eq "1"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "0"
+    expect( page.find(:css, 'span#todo-count').text ).to eq "1"
+  end
   def make_new_task description
-    visit todos_path
     fill_in 'new-todo', with: description
     page.execute_script("$('form').submit()")
   end
